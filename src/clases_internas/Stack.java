3c13cec730d6fb977b5ec040dc0545a6f9ac83fc
plucero <plucero87@gmail.com>
2015-04-01 15:20:20 -0300

    add esqueleto de la clase Stack

---
+++ src/clases_internas/Stack.java
@@ -0,0 +1,46 @@
+package clases_internas;
+
+public class Stack {
+
+    private Node first;
+    private int quantity;
+
+
+    public void push() {
+        // TODO:
+    }
+
+    public String pop() {
+        // TODO:
+        return null;
+    }
+
+    public String peek() {
+        // TODO:
+        return null;
+    }
+
+    public boolean isEmpty() {
+        // TODO:
+        return false;
+    }
+
+    public void show() {
+        // TODO:
+    }
+
+    // clase interna
+    public class Node {
+
+        private String value;
+        private Node next;
+
+        public Node(String value) {
+
+        }
+
+        public Node(String value, Node next) {
+
+        }
+    }
+}
