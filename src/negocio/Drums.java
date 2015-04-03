8a8100325d4831f809ac713cb8f850de47a04d5e
Pablo Lucero <plucero87@gmail.com>
2015-04-03 15:52:31 -0300

    move equals & hashCode to bottom classes
    - ElectricGuitar & ElectricBassGuitar instead of StringedInstrument
    - Drums instead of PercussionInstrument

--- src/negocio/Drums.java
+++ src/negocio/Drums.java
@@ -17,4 +17,27 @@ public class Drums extends PercussionInstrument {
     @Override public void play() {
         System.out.println(name + " drum of " + numberOfCymbals + " cymbals and " + numberOfDrums + " drums");
     }
+
+    @Override public boolean equals(Object obj) {
+
+        if (obj == null || !(obj instanceof Drums))
+            return false;
+        if (obj == this)
+            return true;
+        return (this.numberOfCymbals == ((PercussionInstrument) obj).numberOfCymbals
+                && this.numberOfDrums == ((PercussionInstrument) obj).numberOfDrums
+                && this.name.equals(((PercussionInstrument) obj).name));
+    }
+
+    @Override public int hashCode() {
+        // 17 y 37 son numeros primos definidos arbitrariamente por mi, podria haber usado cualquier otro o, idealmente,
+        // alguna libreria como la clase HashCodeBuilder de Apache Commons Lang
+        int result = 17;
+        result = 37*result + numberOfCymbals;
+        result = 37*result + numberOfDrums;
+        result = 37*result + name.hashCode();
+        result = 37*result + getClass().hashCode();
+
+        return result;
+    }
 }
--- src/negocio/ElectricGuitar.java
+++ src/negocio/ElectricGuitar.java
@@ -36,4 +36,25 @@ public class ElectricGuitar extends StringedInstrument implements Pluggable {
         this.plug = plug;
     }
 
+    @Override public boolean equals(Object obj) {
+
+        if (obj == null || !(obj instanceof ElectricGuitar))
+            return false;
+        if (obj == this)
+            return true;
+        return (this.numberOfStrings == ((ElectricGuitar) obj).numberOfStrings
+                && this.name.equals(((ElectricGuitar) obj).name));
+    }
+
+    @Override public int hashCode() {
+        // 17 y 37 son numeros primos definidos arbitrariamente por mi, podria haber usado cualquier otro o, idealmente,
+        // alguna libreria como la clase HashCodeBuilder de Apache Commons Lang
+        int result = 17;
+        result = 37*result + numberOfStrings;
+        result = 37*result + name.hashCode();
+        result = 37*result + getClass().hashCode();
+
+        return result;
+    }
+
 }
