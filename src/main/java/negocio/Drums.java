a0347d9b0d4d69b829967e32e4d2ac412781a43f
Pablo Lucero <plucero87@gmail.com>
2015-04-06 01:45:27 -0300

    use Apache Equals & HashCode builders

--- src/main/java/negocio/Drums.java
+++ src/main/java/negocio/Drums.java
@@ -1,5 +1,8 @@
 package negocio;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class Drums extends PercussionInstrument {
 
     public Drums() {
@@ -18,26 +21,48 @@ public class Drums extends PercussionInstrument {
         System.out.println(name + " drum of " + numberOfCymbals + " cymbals and " + numberOfDrums + " drums");
     }
 
-    @Override public boolean equals(Object obj) {
+//    @Override public boolean equals(Object obj) {
+//
+//        if (obj == null || !(obj instanceof Drums))
+//            return false;
+//        if (obj == this)
+//            return true;
+//        return (this.numberOfCymbals == ((PercussionInstrument) obj).numberOfCymbals
+//                && this.numberOfDrums == ((PercussionInstrument) obj).numberOfDrums
+//                && this.name.equals(((PercussionInstrument) obj).name));
+//    }
+//
+//    @Override public int hashCode() {
+//        // 17 y 37 son numeros primos definidos arbitrariamente por mi, podria haber usado cualquier otro o, idealmente,
+//        // alguna libreria como la clase HashCodeBuilder de Apache Commons Lang
+//        int result = 17;
+//        result = 37*result + numberOfCymbals;
+//        result = 37*result + numberOfDrums;
+//        result = 37*result + name.hashCode();
+//        result = 37*result + getClass().hashCode();
+//
+//        return result;
+//    }
 
-        if (obj == null || !(obj instanceof Drums))
-            return false;
-        if (obj == this)
-            return true;
-        return (this.numberOfCymbals == ((PercussionInstrument) obj).numberOfCymbals
-                && this.numberOfDrums == ((PercussionInstrument) obj).numberOfDrums
-                && this.name.equals(((PercussionInstrument) obj).name));
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Drums) {
+            Drums other = (Drums) obj;
+            EqualsBuilder builder = new EqualsBuilder();
+            builder.append(name, other.name);
+            builder.append(numberOfCymbals, other.numberOfCymbals);
+            builder.append(numberOfDrums, other.numberOfDrums);
+            return builder.isEquals();
+        }
+        return false;
     }
 
-    @Override public int hashCode() {
-        // 17 y 37 son numeros primos definidos arbitrariamente por mi, podria haber usado cualquier otro o, idealmente,
-        // alguna libreria como la clase HashCodeBuilder de Apache Commons Lang
-        int result = 17;
-        result = 37*result + numberOfCymbals;
-        result = 37*result + numberOfDrums;
-        result = 37*result + name.hashCode();
-        result = 37*result + getClass().hashCode();
-
-        return result;
+    @Override
+    public int hashCode() {
+        HashCodeBuilder builder = new HashCodeBuilder();
+        builder.append(name);
+        builder.append(numberOfCymbals);
+        builder.append(numberOfDrums);
+        return builder.toHashCode();
     }
 }
--- src/main/java/negocio/ElectricBassGuitar.java
+++ src/main/java/negocio/ElectricBassGuitar.java
@@ -1,5 +1,8 @@
 package negocio;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class ElectricBassGuitar extends StringedInstrument implements Pluggable {
 
     private PlugType plug = PlugType.TEN_VOLTS;
@@ -36,25 +39,45 @@ public class ElectricBassGuitar extends StringedInstrument implements Pluggable 
         this.plug = plug;
     }
 
-    @Override public boolean equals(Object obj) {
+//    @Override public boolean equals(Object obj) {
+//
+//        if (obj == null || !(obj instanceof ElectricBassGuitar))
+//            return false;
+//        if (obj == this)
+//            return true;
+//        return (this.numberOfStrings == ((ElectricBassGuitar) obj).numberOfStrings
+//                && this.name.equals(((ElectricBassGuitar) obj).name));
+//    }
+//
+//    @Override public int hashCode() {
+//        // 17 y 37 son numeros primos definidos arbitrariamente por mi, podria haber usado cualquier otro o, idealmente,
+//        // alguna libreria como la clase HashCodeBuilder de Apache Commons Lang
+//        int result = 17;
+//        result = 37*result + numberOfStrings;
+//        result = 37*result + name.hashCode();
+//        result = 37*result + getClass().hashCode();
+//
+//        return result;
+//    }
 
-        if (obj == null || !(obj instanceof ElectricBassGuitar))
-            return false;
-        if (obj == this)
-            return true;
-        return (this.numberOfStrings == ((ElectricBassGuitar) obj).numberOfStrings
-                && this.name.equals(((ElectricBassGuitar) obj).name));
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ElectricBassGuitar) {
+            ElectricBassGuitar other = (ElectricBassGuitar) obj;
+            EqualsBuilder builder = new EqualsBuilder();
+            builder.append(name, other.name);
+            builder.append(numberOfStrings, other.numberOfStrings);
+            return builder.isEquals();
+        }
+        return false;
     }
 
-    @Override public int hashCode() {
-        // 17 y 37 son numeros primos definidos arbitrariamente por mi, podria haber usado cualquier otro o, idealmente,
-        // alguna libreria como la clase HashCodeBuilder de Apache Commons Lang
-        int result = 17;
-        result = 37*result + numberOfStrings;
-        result = 37*result + name.hashCode();
-        result = 37*result + getClass().hashCode();
-
-        return result;
+    @Override
+    public int hashCode() {
+        HashCodeBuilder builder = new HashCodeBuilder();
+        builder.append(name);
+        builder.append(numberOfStrings);
+        return builder.toHashCode();
     }
 
 }
